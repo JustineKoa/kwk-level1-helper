958873d54d05cfb855e4856bfd23e7d57edb438a
Justine Koa <justinekoa@gmail.com>
2018-07-03 09:35:13 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+
+get '/' do
+    erb :index
+end
+
+get '/command_line' do
+    erb :command_line
+end
+
+get '/ruby' do
+    erb :ruby
+end
+
+get '/html' do
+    erb :html
+end
+
+get '/css' do
+    erb :css
+end
+
+get '/bootstrap' do
+    erb :bootstrap
+end
+
+get '/ui_gradients' do
+    erb :ui_gradients
+end
+
+get '/google_fonts' do
+    erb :google_fonts
+end
+
+get '/icons' do
+    erb :icons
+end
+
+get '/sinatra' do
+    erb :sinatra
+end
+
+get '/advice' do
+    erb :advice
+end
+
+
+
+
+
+
+
